544de78a523703950a23681b506a6426b9505803
Faithfulzp <fzp_hangzhou@163.com>
2022-08-24 16:11:47 +0800

    首页的折线图完成

---
+++ 后台管理系统/src/views/dashboard/Card/LineCard/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="charts" ref="lineCharts"></div>
+</template>
+
+<script>
+import * as echarts from "echarts";
+export default {
+  name: "lineCard",
+  mounted() {
+    // 初始化echarts实例
+    let lineCharts = echarts.init(this.$refs.lineCharts);
+    // 配置数据
+    lineCharts.setOption({
+      xAxis: {
+        // 关闭x轴
+        show: false,
+        type: "category",
+      },
+      yAxis: {
+        // 关闭y轴
+        show: false,
+      },
+      series: [
+        {
+          type: "line",
+          data: [31, 40, 36, 75, 48, 53, 14, 24, 34],
+          // 线条样式
+          lineStyle: {
+            color: "purple",
+          },
+          // 点的样式
+          itemStyle: {
+            // 全透明
+            opacity: 0,
+          },
+          // 区域填充颜色设置
+          areaStyle: {
+            // 线性渐变，前四个参数分别是 x0, y0, x2, y2, 范围从 0 - 1，相当于在图形包围盒中的百分比，如果 globalCoord 为 `true`，则该四个值是绝对的像素位置
+            color: {
+              type: "linear",
+              x: 0,
+              y: 0,
+              x2: 0,
+              y2: 1,
+              colorStops: [
+                {
+                  offset: 0,
+                  color: "purple", // 0% 处的颜色
+                },
+                {
+                  offset: 1,
+                  color: "#fff", // 100% 处的颜色
+                },
+              ],
+              global: false, // 缺省为 false
+            },
+          },
+          // 圆滑模式
+          smooth: true,
+        },
+      ],
+      // 布局调整，把一些缝隙给去除
+      grid: {
+        left: 0,
+        right: 0,
+        top: 0,
+        bottom: 0,
+      },
+    });
+  },
+};
+</script>
+
+<style scoped>
+.charts {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ 后台管理系统/src/views/dashboard/Card/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="6">
+        <el-card>
+          <Detail title="总销售额" count="￥ 126560">
+            <template slot="charts">
+              <span>周同比 56.67%</span>
+              <i class="el-icon-caret-top" style="color: rgb(225, 54, 20)"></i>
+              <span>日同比 19.16%</span>
+              <i
+                class="el-icon-caret-bottom"
+                style="color: rgb(37, 207, 56)"
+              ></i>
+            </template>
+            <template slot="footer">
+              <span>日销售额￥ 12612</span>
+            </template>
+          </Detail>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Detail title="访问量" count="88460">
+            <template slot="charts">
+              <lineCard></lineCard>
+            </template>
+            <template slot="footer">
+              <span>日访问量 1262</span>
+            </template>
+          </Detail>
+        </el-card>
+      </el-col>
+      <el-col :span="6">
+        <el-card>
+          <Detail title="支付笔数" count="88460">
+            <template slot="charts"> </template>
+            <template slot="footer">
+              <span>转化率 65%</span>
+            </template>
+          </Detail>
+        </el-card> </el-col
+      ><el-col :span="6">
+        <el-card>
+          <Detail title="运营活动效果" count="76%">
+            <template slot="charts"> </template>
+            <template slot="footer">
+              <span>周同比 12%</span>
+              <i class="el-icon-caret-top" style="color: rgb(225, 54, 20)"></i>
+              <span>日同比 11%</span>
+              <i
+                class="el-icon-caret-bottom"
+                style="color: rgb(37, 207, 56)"
+              ></i>
+            </template>
+          </Detail>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Detail from "./Detail";
+import lineCard from "./LineCard";
+export default {
+  name: "Card",
+  components: {
+    Detail,
+    lineCard,
+  },
+};
+</script>
+
+<style>
+</style>
