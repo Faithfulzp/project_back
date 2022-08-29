d5e1f14f4ca40b06424f3e906a8d8fba486e0324
Faithfulzp <fzp_hangzhou@163.com>
2022-08-29 17:15:12 +0800

    将echarts图表放到el-tab-pane外边了，其实代码更加简洁

--- 后台管理系统/src/views/dashboard/Sale/index.vue
+++ 后台管理系统/src/views/dashboard/Sale/index.vue
@@ -3,104 +3,12 @@
     <el-card class="box-card" style="margin: 10px 0">
       <div slot="header" class="clearfix">
         <!-- @tab-click="handleClickTab" -->
-        <!-- 左侧内容 -->
+        <!-- tab标签页内容 -->
         <el-tabs v-model="activeName" class="tabs" @tab-click="handleClickTab">
-          <el-tab-pane label="销售额" name="sale">
-            <el-row :gutter="10" v-if="activeName == 'sale'">
-              <el-col :span="18" class="col_18">
-                <div class="charts" ref="charts"></div>
-              </el-col>
-              <el-col :span="6" class="col_6">
-                <div style="font-size: 14px">门店销售额排名</div>
-                <ul>
-                  <li>
-                    <span class="rindex">1</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span class="rindex">2</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span class="rindex">3</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span>4</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span>5</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span>6</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span>7</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                </ul>
-              </el-col>
-            </el-row>
-          </el-tab-pane>
-          <el-tab-pane label="访问量" name="visits">
-            <el-row :gutter="10" v-if="activeName == 'visits'">
-              <el-col :span="18" class="col_18">
-                <div class="charts" ref="charts"></div>
-              </el-col>
-              <el-col :span="6" class="col_6">
-                <div style="font-size: 14px">门店{{ rankName }}排名</div>
-                <ul>
-                  <li>
-                    <span class="rindex">1</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span class="rindex">2</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span class="rindex">3</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span>4</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span>5</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span>6</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                  <li>
-                    <span>7</span>
-                    <span>肯德基</span>
-                    <span class="rvalue">123,223</span>
-                  </li>
-                </ul>
-              </el-col>
-            </el-row>
-          </el-tab-pane>
+          <el-tab-pane label="销售额" name="sale"> </el-tab-pane>
+          <el-tab-pane label="访问量" name="visits"></el-tab-pane>
         </el-tabs>
-        <!-- 右侧内容 -->
+        <!-- 顶部右侧内容 -->
         <div class="right">
           <span @click="setDay">本日</span>
           <span @click="setWeek">本周</span>
@@ -123,6 +31,56 @@
           >
           </el-date-picker>
         </div>
+        <!-- tab的内容（不放在el-tab-pane）-->
+      </div>
+      <div>
+        <!-- v-if判断点击的tab -->
+        <el-row :gutter="10">
+          <el-col :span="18" class="col_18">
+            <!-- echarts图表容器 -->
+            <div class="charts" ref="charts"></div>
+          </el-col>
+          <el-col :span="6" class="col_6">
+            <div style="font-size: 14px">门店{{ rankName }}排名</div>
+            <ul>
+              <li>
+                <span class="rindex">1</span>
+                <span>肯德基</span>
+                <span class="rvalue">123,223</span>
+              </li>
+              <li>
+                <span class="rindex">2</span>
+                <span>肯德基</span>
+                <span class="rvalue">123,223</span>
+              </li>
+              <li>
+                <span class="rindex">3</span>
+                <span>肯德基</span>
+                <span class="rvalue">123,223</span>
+              </li>
+              <li>
+                <span>4</span>
+                <span>肯德基</span>
+                <span class="rvalue">123,223</span>
+              </li>
+              <li>
+                <span>5</span>
+                <span>肯德基</span>
+                <span class="rvalue">123,223</span>
+              </li>
+              <li>
+                <span>6</span>
+                <span>肯德基</span>
+                <span class="rvalue">123,223</span>
+              </li>
+              <li>
+                <span>7</span>
+                <span>肯德基</span>
+                <span class="rvalue">123,223</span>
+              </li>
+            </ul>
+          </el-col>
+        </el-row>
       </div>
     </el-card>
   </div>
@@ -140,86 +98,77 @@ export default {
       rankName: "销售额",
       // 收集日期
       date: [],
+      // 图表
+      mycharts: null,
     };
   },
   mounted() {
-    this.getEcharts();
+    this.mycharts = echarts.init(this.$refs.charts);
+    this.mycharts.setOption({
+      title: {
+        text: this.title + "趋势",
+      },
+      tooltip: {
+        trigger: "axis",
+        axisPointer: {
+          type: "shadow",
+        },
+      },
+      grid: {
+        left: "3%",
+        right: "4%",
+        bottom: "3%",
+        containLabel: true,
+      },
+      xAxis: [
+        {
+          type: "category",
+          data: [
+            "一月",
+            "二月",
+            "三月",
+            "四月",
+            "五月",
+            "六月",
+            "七月",
+            "八月",
+            "九月",
+            "十月",
+            "十一月",
+            "十二月",
+          ],
+          axisTick: {
+            alignWithLabel: true,
+          },
+        },
+      ],
+      yAxis: [
+        {
+          type: "value",
+        },
+      ],
+      series: [
+        {
+          name: "Direct",
+          type: "bar",
+          barWidth: "60%",
+          data: [10, 52, 200, 334, 390, 330, 220, 212, 123, 12, 33, 44],
+        },
+      ],
+    });
+  },
+  computed: {
+    title() {
+      return this.activeName == "sale" ? "销售额" : "访问量";
+    },
   },
   methods: {
     // echarts图表
-    getEcharts() {
-      this.rankName = "销售额";
-      let mycharts = echarts.init(this.$refs.charts);
-      let option1 = {
-        title: {
-          text: "销售额趋势",
-        },
-        tooltip: {
-          trigger: "axis",
-          axisPointer: {
-            type: "shadow",
-          },
-        },
-        grid: {
-          left: "3%",
-          right: "4%",
-          bottom: "3%",
-          containLabel: true,
-        },
-        xAxis: [
-          {
-            type: "category",
-            data: [
-              "一月",
-              "二月",
-              "三月",
-              "四月",
-              "五月",
-              "六月",
-              "七月",
-              "八月",
-              "九月",
-              "十月",
-              "十一月",
-              "十二月",
-            ],
-            axisTick: {
-              alignWithLabel: true,
-            },
-          },
-        ],
-        yAxis: [
-          {
-            type: "value",
-          },
-        ],
-        series: [
-          {
-            name: "Direct",
-            type: "bar",
-            barWidth: "60%",
-            data: [10, 52, 200, 334, 390, 330, 220, 212, 123, 12, 33, 44],
-          },
-        ],
-      };
-      mycharts.setOption(option1);
-      // tab标签页改变，判断标签页name，并获取已经存在的初始化过的配置,修改其中的数据，再重新进行配置可以避免每切换tab都要初始化echarts
-      if (this.activeName == "visits") {
-        this.rankName = "访问量";
-        let option2 = mycharts.getOption();
-        option2.series[0].data = [
-          100, 152, 233, 334, 350, 497, 226, 252, 133, 42, 13, 74,
-        ];
-        option2.title[0].text = "访问量趋势";
-        mycharts.setOption(option2);
-      }
-    },
+    getEcharts() {},
     // tab标签页的回调
     handleClickTab() {
-      // 等dom更新后再去初始化图表
-      this.$nextTick(() => {
-        this.getEcharts();
-      });
+      // 如果不是放在el-tab-pane中的话就不需要使用nextTick
+      // this.getEcharts();
     },
     // 获取本日日期
     setDay() {
@@ -245,6 +194,37 @@ export default {
       this.date = [start, end];
     },
   },
+  watch: {
+    title() {
+      // tab标签页改变，判断标签页name，并获取已经存在的初始化过的配置,修改其中的数据，再重新进行配置可以避免每切换tab都要初始化echarts
+      if (this.activeName == "visits") {
+        this.rankName = "访问量";
+        this.mycharts.setOption({
+          title: {
+            text: this.title+'趋势',
+          },
+          series: [
+            {
+              data: [100, 152, 233, 334, 350, 497, 226, 252, 133, 42, 13, 74],
+            },
+          ],
+        });
+      }
+      if (this.activeName == "sale") {
+        this.rankName = "销售额";
+        this.mycharts.setOption({
+          title: {
+            text: this.title+'趋势',
+          },
+          series: [
+            {
+              data: [10, 52, 200, 334, 390, 330, 220, 212, 123, 12, 33, 44],
+            },
+          ],
+        });
+      }
+    },
+  },
 };
 </script>
 
@@ -253,6 +233,7 @@ export default {
 ::v-deep .el-card__header {
   border-bottom: none;
 }
+// 顶部样式
 .clearfix {
   position: relative;
   display: flex;
@@ -274,14 +255,16 @@ export default {
   width: 250px;
   margin: 0 20px;
 }
+// 图表样式
 .col_18 {
   height: 300px;
 }
 // 必须设置长宽
-.tabs .charts {
+.charts {
   width: 100%;
   height: 100%;
 }
+// 排行榜样式
 .col_6 ul {
   list-style: none;
   width: 100%;
@@ -294,7 +277,6 @@ export default {
 .col_6 ul li span {
   margin-right: 40px;
 }
-// 右边门店排行样式
 .rindex {
   float: left;
   width: 20px;
